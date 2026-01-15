506e5981af297b5260c23c7fce65f877de8fcdae
jhone-cmd <jhone.1941@gmail.com>
2026-01-15 16:10:36 -0300

    feat: configuring layout

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,17 +1,8 @@
-import { Route } from 'react-router-dom'
-import { Header } from './components/Header'
-import { Sidebar } from './components/Sidebar'
 import './styles/global.css'
+
 import { Routes } from './routes'
 
+
 export function App() {
-  return (
-    <div className='h-screen w-screen text-rotion-100 flex'>
-      <Sidebar />
-      <div className='flex-1 flex flex-col max-h-screen'>
-        <Header />
-        <Routes />
-      </div>
-    </div>
-  )
+  return <Routes />
 }
---
+++ src/renderer/src/pages/layouts/default.tsx
@@ -0,0 +1,15 @@
+import { Sidebar } from "../../components/Sidebar";
+import { Header } from "../../components/Header";
+import { Outlet } from "react-router-dom";
+
+export function Default() {
+  return (
+    <div className='h-screen w-screen text-rotion-100 flex'>
+      <Sidebar />
+      <div className='flex-1 flex flex-col max-h-screen'>
+        <Header />
+        <Outlet />
+      </div>
+    </div>
+  )
+}
