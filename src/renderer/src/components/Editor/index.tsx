d17429efa7899d2b7cc55187f35786856fff9fed
jhone-cmd <jhone.1941@gmail.com>
2026-01-16 10:52:26 -0300

    feat: styling the text editor

--- src/renderer/src/components/Editor/index.tsx
+++ src/renderer/src/components/Editor/index.tsx
@@ -2,16 +2,27 @@ import Typography from "@tiptap/extension-typography";
 import Highlight from "@tiptap/extension-highlight";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { Placeholder } from "@tiptap/extension-placeholder";
+import Document from "@tiptap/extension-document";
 
 export function Editor() {
   const editor = useEditor({
-    extensions: [StarterKit, Highlight, Typography],
-    content: "<p>Hello World!</p>",
+    extensions: [
+      Document.extend({
+        content: "heading block*",
+      }),
+      StarterKit.configure({
+        document: false,
+      }), Highlight, Typography, Placeholder.configure({
+        placeholder: "Untitled",
+        emptyEditorClass: "before:content-[attr(data-placeholder)] before:text-gray-500 before:float-left before:h-0 before:pointer-events-none",
+      })],
+    content: "<h1>Back-End</h1><p>This is your new editor.</p>",
     autofocus: 'end',
     editorProps: {
       attributes: {
         class:
-          "focus:outline-none",
+          "focus:outline-none prose prose-invert prose-headings:mt-0",
       },
     },
   });
