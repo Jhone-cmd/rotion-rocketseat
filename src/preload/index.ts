887050df8f3179c9ee85a732861d3d593e5d3925
jhone-cmd <jhone.1941@gmail.com>
2026-01-19 17:40:42 -0300

    feat: CRUD Structure via IPC

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,7 +1,7 @@
 import { contextBridge, ipcRenderer } from 'electron'
 import { ElectronAPI, electronAPI } from '@electron-toolkit/preload'
 import { IPC } from '@shared/constants/ipc'
-import { FetchAllDocumentsResponse } from '../shared/types/ipc'
+import { CreateDocumentResponse, DeleteDocumentRequest, FetchAllDocumentsResponse, FetchDocumentRequest, FetchDocumentResponse, SaveDocumentRequest } from '../shared/types/ipc'
 
 declare global {
   interface Window {
@@ -12,7 +12,24 @@ declare global {
 const api = {
   fetchDocuments(): Promise<FetchAllDocumentsResponse> {
     return ipcRenderer.invoke(IPC.DOCUMENTS.FETCH_ALL)
-  }
+  },
+
+  fetchDocument(param: FetchDocumentRequest): Promise<FetchDocumentResponse> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.FETCH, param)
+  },
+
+  createDocument(): Promise<CreateDocumentResponse> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.CREATE)
+  },
+
+  saveDocument(document: SaveDocumentRequest): Promise<void> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.SAVE, document)
+  },
+
+  deleteDocument(param: DeleteDocumentRequest): Promise<void> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.DELETE, param)
+  },
+
 }
 
 if (process.contextIsolated) {
