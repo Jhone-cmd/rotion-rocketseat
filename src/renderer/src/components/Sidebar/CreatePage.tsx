bb716b3ddf39be57bcede90236dbc8555540069a
jhone-cmd <jhone.1941@gmail.com>
2026-01-21 10:29:44 -0300

    feat: creating new document

--- src/renderer/src/components/Sidebar/CreatePage.tsx
+++ src/renderer/src/components/Sidebar/CreatePage.tsx
@@ -1,8 +1,29 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { Plus } from 'phosphor-react'
 
 export function CreatePage() {
+  const queryClient = useQueryClient()
+  const { mutateAsync: createDocument, isPending: isCreatingNewDocument } = useMutation({
+    mutationFn: async () => {
+      const response = await window.api.createDocument()
+
+      return response.data
+    },
+    onSuccess: (data) => {
+      queryClient.setQueryData<Document[]>(['documents'], (documents) => {
+        if (documents && documents.length >= 0) {
+          return [...documents, data]
+        }
+        return [data] as any
+      })
+    },
+  })
+
   return (
-    <button className="flex w-[240px] px-5 items-center text-sm gap-2 absolute bottom-0 left-0 right-0 py-4 border-t border-rotion-600 hover:bg-rotion-700 disabled:opacity-60">
+    <button
+      disabled={isCreatingNewDocument}
+      onClick={() => createDocument()}
+      className="flex w-[240px] px-5 items-center text-sm gap-2 absolute bottom-0 left-0 right-0 py-4 border-t border-rotion-600 hover:bg-rotion-700 disabled:opacity-60">
       <Plus className="h-4 w-4" />
       Create new page
     </button>
